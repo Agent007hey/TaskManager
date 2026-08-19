6a92f144bba42d0dba36e0ff9023faeeeec51069
Agent007hey <krishnakd716@gmail.com>
2026-08-19 12:28:23 +0530

    added delete route

--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.middleware.js";
-import { createController , allTaskController , getOneTaskController ,updateTaskController } from "../controllers/task.controller.js";
+import { createController , allTaskController , getOneTaskController ,updateTaskController , deleteTaskController } from "../controllers/task.controller.js";
 
 const taskRouter = Router();
 
@@ -10,6 +10,7 @@ taskRouter.post("/" , createController );
 taskRouter.get("/" , allTaskController);
 taskRouter.get("/:id" , getOneTaskController);
 taskRouter.put("/:id" , updateTaskController);
+taskRouter.delete("/:id" , deleteTaskController);
  
 
 export default taskRouter;
