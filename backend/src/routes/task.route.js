d6667b2f18a68fa44e3b5b91917927bade81d041
Agent007hey <krishnakd716@gmail.com>
2026-08-19 11:32:46 +0530

    added getone task route

--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.middleware.js";
-import { createController , allTaskController , getOneTaskController} from "../controllers/task.controller.js";
+import { createController , allTaskController , getOneTaskController , } from "../controllers/task.controller.js";
 
 const taskRouter = Router();
 
@@ -9,5 +9,6 @@ taskRouter.use(authMiddleware);
 taskRouter.post("/" , createController );
 taskRouter.get("/" , allTaskController);
 taskRouter.get("/:id" , getOneTaskController);
+ 
 
 export default taskRouter;
