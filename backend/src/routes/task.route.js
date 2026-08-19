cf53096981b575e36f498a86867107004ea71146
Agent007hey <krishnakd716@gmail.com>
2026-08-19 12:02:51 +0530

    added put request on updateTaskController

--- backend/src/routes/task.route.js
+++ backend/src/routes/task.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.middleware.js";
-import { createController , allTaskController , getOneTaskController , } from "../controllers/task.controller.js";
+import { createController , allTaskController , getOneTaskController ,updateTaskController } from "../controllers/task.controller.js";
 
 const taskRouter = Router();
 
@@ -9,6 +9,7 @@ taskRouter.use(authMiddleware);
 taskRouter.post("/" , createController );
 taskRouter.get("/" , allTaskController);
 taskRouter.get("/:id" , getOneTaskController);
+taskRouter.put("/:id" , updateTaskController);
  
 
 export default taskRouter;
